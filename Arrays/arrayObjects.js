d34d48853357e2360cd8762e5a8690cf941a609d
Gabster305 <gabrielarios@AIRG.local>
2021-04-08 22:09:56 -0400

    Object inside an Array

---
+++ Arrays/arrayObjects.js
@@ -0,0 +1,21 @@
+//Array with Object Samples Inside//
+let donationSite = [
+  {
+    name: 'Broward',
+    city: 'Sunrise',
+    zipCode: '33190',
+    open: true,
+  },
+  {
+    name: 'North Dade',
+    city: 'Aventura',
+    zipCode: '33180',
+    open: true,
+  },
+  {
+    name: 'Dade South',
+    city: 'Miami',
+    zipCode: '33165',
+    open: false,
+  },
+]
