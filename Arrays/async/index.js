140a7150e9717617773c327ec409ba97c4ae6292
Gabster305 <gabrielarios@AIRG.local>
2021-04-08 21:12:50 -0400

    Array types

---
+++ Arrays/async/index.js
@@ -0,0 +1,6 @@
+//Gabriel Rios. This is my practice JS Array File//
+
+//Array with common data types
+let plants = ['orquid', 'cactus', 'bonsai']
+// Array with different types of data type
+let answers = ['false', 'female', 55, [20]]
